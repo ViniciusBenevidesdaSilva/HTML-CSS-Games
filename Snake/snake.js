462fb8df4450c139b49813080d7472ac406d3c0e
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2023-12-11 19:32:35 -0300

    refactor: Simplificação e organização do código

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,32 +1,27 @@
 
-//board
-var blockSize = 30;
-var cols = 20;
-var rows = 20;
-var board;
-var context;
-var scoreTxt;
+//#region Variables
 
-var score = 0;
-var maxScore = 0;
+//board
+var blockSize = 30, cols = 20, rows = 20;
+var board, context, scoreTxt;
 
 //snake head
 var snakeX =  blockSize * Math.floor(cols / 2);
 var snakeY = blockSize * Math.floor(rows / 2);
-
-var velocityX = 0;
-var velocityY = 0;
-
 var snakeBody = [];
+var velocityX = 0, velocityY = 0;
 
 //food
-var foodX =  blockSize;
-var foodY = blockSize;
+var foodX = blockSize, foodY = blockSize;
 
+//game
 var gameOver = false;
+var score = 0, maxScore = 0;
 
+//#endregion Variables
 
-window.onload = function() {
+//#region Main
+document.addEventListener("DOMContentLoaded", function () {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
@@ -34,57 +29,95 @@ window.onload = function() {
 
     scoreTxt = document.getElementById("score");
 
-
     placeFood();
     document.addEventListener("keyup", startGame);
     document.addEventListener("keyup", changeDirection);
     
     setInterval(update, 1000/10);  // 100ms
-}
+});
 
 function startGame() {
+
     if(!gameOver){
         return;
     }
-    score = 0;
-    
-    snakeX =  blockSize * Math.floor(cols / 2);
-    snakeY = blockSize * Math.floor(rows / 2);
 
+    initializeGame();
+}
+
+function initializeGame() {
+    score = 0;
+    snakeX = blockSize * Math.floor(cols / 2);
+    snakeY = blockSize * Math.floor(rows / 2);
     velocityX = 0;
     velocityY = 0;
-
     snakeBody = [];
-
     placeFood();
-
     gameOver = false;
 }
 
 function update() {
-    exibeScore();
-
     if(gameOver){
         return;
     }
 
-    //background
-    context.fillStyle = "black";
-    context.fillRect(0, 0, board.width, board.height);
-
-    //food
-    context.fillStyle = "red";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
-
+    drawBackground();
+    drawFood();
     verifyEat();
-
     moveSnake();
+
+    drawSnake();
+    drawScore();
 
     verifyGameOver();
 }
 
-function exibeScore(){
+//#endregion Main
 
+//#region Utils
+
+function drawBlock(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x, y, blockSize, blockSize);
+}
+
+function calcularCoresIntermediarias(corInicial, corFinal, quantidade) {
+    const coresIntermediarias = [];
+    
+    for (let i = 0; i <= quantidade; i++) {
+      const r = Math.round(corInicial.r + (corFinal.r - corInicial.r) * (i / quantidade));
+      const g = Math.round(corInicial.g + (corFinal.g - corInicial.g) * (i / quantidade));
+      const b = Math.round(corInicial.b + (corFinal.b - corInicial.b) * (i / quantidade));
+  
+      coresIntermediarias.push(`rgb(${r}, ${g}, ${b})`);
+    }
+    
+    return coresIntermediarias;
+}
+
+function drawBackground() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+}
+
+function drawFood() {
+    drawBlock(foodX, foodY, "red");
+}
+
+function drawSnake() {
+    const corInicial = { r: 0, g: 255, b: 0 }; // #00FF00
+    const corFinal = { r: 0, g: 238, b: 255 }; // #00EEFF
+
+    let cores = calcularCoresIntermediarias(corInicial, corFinal, 1 + snakeBody.length);
+
+    drawBlock(snakeX, snakeY, cores[0]);
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        drawBlock(snakeBody[i][0], snakeBody[i][1], cores[i + 1]);
+    }
+}
+
+function drawScore() {
     if(score > maxScore){
         maxScore = score;
     }
@@ -92,6 +125,9 @@ function exibeScore(){
     scoreTxt.innerHTML = `Score: ${score}    Max Score: ${maxScore}`;
 }
 
+//#endregion Utils
+
+//#region Actions
 
 function placeFood() {
     do {
@@ -101,29 +137,33 @@ function placeFood() {
 }
 
 function changeDirection(e) {
-    if(e.code == "ArrowUp" && velocityY != 1){
-        velocityX = 0;
-        velocityY = -1;
-    }
-    else if(e.code == "ArrowDown" && velocityY != -1){
-        velocityX = 0;
-        velocityY = 1;
-    }    
-    else if(e.code == "ArrowLeft" && velocityX != 1){
-        velocityX = -1;
-        velocityY = 0;
-    }    
-    else if(e.code == "ArrowRight" && velocityX != -1){
-        velocityX = 1;
-        velocityY = 0;
-    }
-}
+    if (gameOver) return;
 
-function verifyEat() {
-    if(foodX == snakeX && foodY == snakeY){
-        snakeBody.push([foodX, foodY]);
-        score++;
-        placeFood();
+    switch (e.code) {
+        case "ArrowUp":
+            if (velocityY !== 1) {
+                velocityX = 0;
+                velocityY = -1;
+            }
+            break;
+        case "ArrowDown":
+            if (velocityY !== -1) {
+                velocityX = 0;
+                velocityY = 1;
+            }
+            break;
+        case "ArrowLeft":
+            if (velocityX !== 1) {
+                velocityX = -1;
+                velocityY = 0;
+            }
+            break;
+        case "ArrowRight":
+            if (velocityX !== -1) {
+                velocityX = 1;
+                velocityY = 0;
+            }
+            break;
     }
 }
 
@@ -150,9 +190,22 @@ function moveSnake() {
     }
 }
 
+function verifyEat() {
+    if(foodX === snakeX && foodY === snakeY){
+        snakeBody.push([foodX, foodY]);
+        score++;
+        placeFood();
+    }
+}
+
 function verifyGameOver() {
     //gameOver conditions
-    if(snakeX < 0 || snakeX >= cols * blockSize || snakeY < 0 || snakeY >= rows * blockSize){
+    if(
+        snakeX < 0 || 
+        snakeX >= cols * blockSize || 
+        snakeY < 0 || 
+        snakeY >= rows * blockSize
+    ){
         gameOver = true;
     }
 
@@ -166,4 +219,6 @@ function verifyGameOver() {
         alert("Game Over!");
     }
 }
+
+//#endregion Actions
 
